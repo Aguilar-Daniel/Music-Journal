0735a9b50a911e7a839a79745776f862a83f7f50
Daniel Aguilar <daniel.x2822@gmail.com>
2026-06-20 20:37:06 -0400

    Add valid_input() function to validate menu and song input

--- Main.py
+++ Main.py
@@ -6,6 +6,16 @@ def save_journal(journal_data):
     with open("Notes_and_Ratings.json", "w") as file:
         json.dump(journal_data, file, indent=4)
 
+def valid_input(prompt, valid_options):
+    while True:
+        try:
+            choice = int(input(prompt))
+            if choice in valid_options:
+                return choice
+            else:
+                print("Pick a valid option.")
+        except ValueError:
+            print("Pick a valid option.")
 
 with open("config.json", "r") as a:
     key = json.load(a)
@@ -21,9 +31,7 @@ def confirm_song():
         for x, track in enumerate(tracks, start=1):
             print(str(x) + "."  + track["name"] + " - " + track["artist"]["#text"])
 
-        select = int(input("Pick a song to rate or add notes to: "))
-        while select < 1 or select > len(tracks):
-            select = int(input("Pick pick a valid song"))
+        select = valid_input("Pick a song to rate or add notes to: ", list(range(1, 11)))
 
         print("You chose: " + tracks[select - 1]["name"])
         confirm = input("Confirm? (Yes/No): ").lower()
@@ -74,25 +82,21 @@ with open("Songs.json", "w") as f:
 save_journal(journal_data=journal)
 
 run = True
+
 while run:
 
     i = confirm_song()
 
     song = tracks[i - 1]["name"]
-    menu = int(input("MAIN MENU\n1.Edit note(s)\n2.Rate\n3.Pick another song\n4.Quit\n"))
+    menu = valid_input("MAIN MENU\n1.Edit note(s)\n2.Rate\n3.Pick another song\n4.Quit\n", [1, 2, 3, 4])
 
-    while menu not in [1, 2, 3, 4]:
-        menu = int(input("Pick a valid option!\n 1.Edit note(s)\n 2.Rate\n"))
 
     while menu != 3 and menu != 4:
 
         if menu == 1:
             print("Current note(s): " + journal[song]["Notes"])
-            note_Edit = int(input("1.Add to note\n2.Rewrite note\n3.Back\n"))
+            note_Edit = valid_input("1.Add to note\n2.Rewrite note\n3.Back\n", [1, 2, 3])
 
-            while note_Edit not in [1, 2, 3]:
-                print("Please select a valid option")
-                note_Edit = int(input("1.Add to note\n2.Rewrite note\n3.Back\n"))
             if note_Edit == 1:
                 new_Note = input("Add to note: ")
                 journal[song]["Notes"] += " " + new_Note
@@ -112,23 +116,19 @@ while run:
                     save_journal(journal_data=journal)
 
             else:
-                menu = int(input("MAIN MENU\n1.Edit note(s)\n2.Rate\n3.Pick another song\n4.Quit\n"))
+                menu = valid_input("MAIN MENU\n1.Edit note(s)\n2.Rate\n3.Pick another song\n4.Quit\n", [1, 2, 3, 4])
 
-                while menu not in [1, 2, 3, 4]:
-                    menu = int(input("Pick a valid option!\n 1.Edit note(s)\n 2.Rate\n3.Pick another song\n4.Quit\n"))
 
 
 
         elif menu == 2:
             print("Current rating: " + str(journal[song]["Rating"]))
 
-            rating = input("Rate out of 10: ")
+            rating = valid_input("Rate out of 10: ", list(range(1, 11)))
             journal[song]["Rating"] = rating
             save_journal(journal_data=journal)
 
-            menu = int(input("MAIN MENU\n1.Edit note(s)\n2.Rate\n3.Pick another song\n4.Quit\n"))
-            while menu not in [1, 2, 3, 4]:
-                menu = int(input("Pick a valid option!\n 1.Edit note(s)\n 2.Rate\n3.Pick another song\n4.Quit\n"))
+            menu = valid_input("MAIN MENU\n1.Edit note(s)\n2.Rate\n3.Pick another song\n4.Quit\n", [1, 2, 3, 4])
 
     if menu == 3:
         continue
